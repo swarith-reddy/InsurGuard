b71bbdd624d3a04b73d906423ed8562a32522b8d
Shrey Jain <jainshreyp@gmail.com>
2020-09-12 13:58:47 -0400

    hello

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import socket
 
-app = Flask(__name__)
 
-@app.route("/",methods=['GET'])
+app = Flask(__name__.split('.')[0])
+
+
+@app.route("/",methods=['GET','POST'])
 def func():
-    return "testing"
+    if request.method == 'GET':
+        return "get request"
+    elif request.method == 'POST':
+        bod = request.get_json()
+        return bod
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(host=socket.gethostbyname(socket.gethostname()), port=8000, debug=True)
