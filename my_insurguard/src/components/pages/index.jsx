8ad7cf4be1610995bbe4792ed1dfe498d3a08c54
samaynandwana <samaynandwana@gmail.com>
2020-09-12 20:20:28 -0400

    Committing Front End

---
+++ my_insurguard/src/components/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from "react"; 
+
+
+const MainPage = () => {
+
+    return(
+
+        <div>
+
+            <h3> Welcome </h3>
+            <small> Main Page</small>
+        </div>
+    )
+}
+
+
+export default MainPage; 
---
+++ my_insurguard/src/components/sections/Hero.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { SectionProps } from '../../utils/SectionProps';
+import ButtonGroup from '../elements/ButtonGroup';
+import Button from '../elements/Button';
+import Image from '../elements/Image';
+import Modal from '../elements/Modal';
+
+const propTypes = {
+  ...SectionProps.types
+}
+
+const defaultProps = {
+  ...SectionProps.defaults
+}
+
+const Hero = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+}) => {
+
+  const [videoModalActive, setVideomodalactive] = useState(false);
+
+  const openModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(true);
+  }
+
+  const closeModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(false);
+  }   
+
+  const outerClasses = classNames(
+    'hero section center-content',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'hero-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container-sm">
+        <div className={innerClasses}>
+          <div className="hero-content">
+            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+              INSURGUARD: <span className="text-color-primary">Your Smart Insurance Solution</span>
+            </h1>
+            <div className="container-xs">
+              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+                  It is not easy to keep track of everything when you are affected by a natural disaster, and spending
+                  hours searching for insurance is the last thing that you want to do. Insureguard is here to help with that.
+
+                </p>
+              <div className="reveal-from-bottom" data-reveal-delay="600">
+                <ButtonGroup>
+                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                    Get Started
+                    </Button>
+                  <Button tag="a" color="dark" wideMobile href="https://devpost.com/software/insurguard-hlrzs9">
+                    Contact the Developers
+                    </Button>
+                </ButtonGroup>
+              </div>
+            </div>
+          </div>
+        
+          <Modal
+            id="video-modal"
+            show={videoModalActive}
+            handleClose={closeModal}
+            video="https://player.vimeo.com/video/174002812"
+            videoTag="iframe" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
