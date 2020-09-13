c9bbb3b936a71ce1757b8c914831670ed1e5da89
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-09-13 10:17:11 -0400

    center buttons

--- my_insurtech/src/views/Fire.js
+++ my_insurtech/src/views/Fire.js
@@ -4,17 +4,17 @@ import NavLink from 'react-router-dom'
 
 function Fire() {
     function req(state) {
-        axios.get
+        return "hello"
     }
-    return(
-        <div className="object-center">
+        return (
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
             <label>
                 State Abbreviation:
             </label>
             <input type="field" name="state"/>
             <input type="submit" value="submit" onSubmit={req()}/>
-        </div>
-    )
+            </div>
+          );
 }
 
 export default Fire
